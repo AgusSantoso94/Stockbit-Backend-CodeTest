6263cf9007324660a2049601267cec4f7e299238
agusyang <agus.santoso@bizzy.co.id>
2021-10-16 00:38:28 +0700

    add answer for question No. 3 Code Refactoring

--- 3.CodeRefactoring/index.js
+++ 3.CodeRefactoring/index.js
@@ -22,7 +22,27 @@ function findFirstStringInBracket(str){
   }
 }
 
+function refactoreFindFirstStringInBracket(str) {
+  const isNotEmpty = str.length > 0 ? true : false;
+  const isBracketsExist = str.includes('(') && str.includes(')');
+  
+  if (isNotEmpty && isBracketsExist) {
+    const indexFirstBracketFound = str.indexOf("(");
+    const wordsAfterFirstBracket = str.substring(indexFirstBracketFound + 1);
+    const indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
+
+    if (indexFirstBracketFound >= 0 && indexClosingBracketFound >= 0) {
+      return wordsAfterFirstBracket.substring(0, indexClosingBracketFound);
+    } else {
+      return '';
+    }
+  } else {
+    return '';
+  }
+}
+
 
 // show result
 const string = 'Every journey begins with a (single) step. We just have to have patience';
 console.log('Before Refactor: ', findFirstStringInBracket(string));
+console.log('After Refactor: ', refactoreFindFirstStringInBracket(string));
