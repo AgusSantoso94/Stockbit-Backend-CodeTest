06554a2c079a8d5a4fb3985b49030cbdabd46cbb
agusyang <agus.santoso@bizzy.co.id>
2021-10-17 00:16:42 +0700

    add common used library and init express

---
+++ 2.SmallExpressJsServer/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Simple Express Server is Working" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
